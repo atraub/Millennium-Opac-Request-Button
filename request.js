28dc5bb408b45d35fc84b231fd0649775fdfcc60
Adam Traub <traubie@gmail.com>
2015-03-23 16:09:04 -0400

    Now with IFs
    IF statements to handle various statuses like Holds.  Also includes date
    comparison to change link to DocDel if dueDate is greater than 4 days.

--- request.js
+++ request.js
@@ -5,37 +5,9 @@ Redirect to appropriate services
 
 */
 
-/* Silly Variables */
-
-var newLink;
-var dueDate, ddDay, ddMonth, ddYear, todayDate, millisPerDay, millisBetween, daysBetween;
-
-/* Date Magic */
-
-dueDate = $("td:contains('DUE')").text().trim().replace("DUE ", "");
-ddMonth = dueDate.split("-")[0]-1; //Base 0
-ddDay = dueDate.split("-")[1];
-ddYear = 20+dueDate.split("-")[2]; //Concatenating "20" to make base year 2000
-
-dueDate = new Date(ddYear,ddMonth,ddDay);
-todayDate = new Date();
-
-console.log(dueDate);
-console.log(todayDate);
-
 /* 
-	Days between method from:
-		http://stackoverflow.com/questions/1036742/date-difference-in-javascript-ignoring-time-of-day
-*/
 
-dueDate = Date.UTC(dueDate.getFullYear(), dueDate.getMonth(), dueDate.getDate());
-todayDate = Date.UTC(todayDate.getFullYear(), todayDate.getMonth(), todayDate.getDate());
-
-daysBetween = Math.abs(dueDate-todayDate)/1000/60/60/24;
-
-console.log(daysBetween);
-
-/* 
+Pseudo-Code
 
 	Checked Out:
 		No Holds Present:  
@@ -49,8 +21,50 @@ console.log(daysBetween);
 
 */
 
+/* Silly Variables */
+
+var newLink = "nope";
+var dueDate, ddDay, ddMonth, ddYear, todayDate, millisPerDay, millisBetween, daysBetween;
+
+/* Date Magic */
+
+if($("td:contains('HOLD')").text()){
+
+	newLink = "http://ill.rit.edu"; //GO TO ILL/CNY
+
+} else if($("td:contains('DUE')").text()){
+
+	dueDate = $("td:contains('DUE')").text().trim().replace("DUE ", "");
+	ddMonth = dueDate.split("-")[0]-1; //Base 0
+	ddDay = dueDate.split("-")[1];
+	ddYear = 20+dueDate.split("-")[2]; //Concatenating "20" to make base year 2000
+
+	dueDate = new Date(ddYear,ddMonth,ddDay);
+	todayDate = new Date();
+
+	console.log(dueDate);
+	console.log(todayDate);
+
+	/* 
+		Days between method from:
+			http://stackoverflow.com/questions/1036742/date-difference-in-javascript-ignoring-time-of-day
+	*/
+
+	dueDate = Date.UTC(dueDate.getFullYear(), dueDate.getMonth(), dueDate.getDate());
+	todayDate = Date.UTC(todayDate.getFullYear(), todayDate.getMonth(), todayDate.getDate());
+
+	daysBetween = Math.abs(dueDate-todayDate)/1000/60/60/24;
+
+	console.log(daysBetween);
+
+	if(daysBetween > 4){
+		newLink = "http://ill.rit.edu"; //GO TO ILL/CNY
+	}
+}
+
+
 /* Link Switching Magic */
 
-newLink = "http://library.rit.edu";
-
-$("[href*='request~']").attr("href", newLink);
+if(newLink != "nope"){
+	$("[href*='request~']").attr("href", newLink);
+}
